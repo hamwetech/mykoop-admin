a3cfc61dbbc215e42b8e97b29f0587cae0f5b13c
root <tech@hamwe.org>
2021-01-26 13:57:21 +0300

    Dash districts filter

--- dashboard/views.py
+++ dashboard/views.py
@@ -88,6 +88,7 @@ class DashboardView(TemplateView):
         context['youth'] = youth
         context['refugee'] = rc
         context['acreage'] = acreage
+        context['farmers'] = farmers
         return context
     
     
